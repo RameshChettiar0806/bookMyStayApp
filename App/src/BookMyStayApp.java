8910714e0acff03066b190bb2230b72124ba9557
RameshChettiar0806 <rameshchettiar06@gmail.com>
2026-03-17 15:16:35 +0530

    UC3 - Centralized Room Inventory Management

--- App/src/BookMyStayApp.java
+++ App/src/BookMyStayApp.java
@@ -1,47 +1,36 @@
-/**
- * MAIN CLASS - BookMyStayApp
- *
- * Entry point of the application.
- *
- * @version 2.1
- */
 public class BookMyStayApp {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        System.out.println("=======================================");
-        System.out.println("     Welcome to Book My Stay App       ");
-        System.out.println("     Hotel Booking System v1.0         ");
-        System.out.println("=======================================\n");
+        System.out.println("Hotel Room Inventory Status\n");
 
-        // Create room objects
+        // Room objects
         Room singleRoom = new SingleRoom();
         Room doubleRoom = new DoubleRoom();
         Room suiteRoom = new SuiteRoom();
 
-        // Static availability
-        int singleRoomAvailable = 5;
-        int doubleRoomAvailable = 3;
-        int suiteRoomAvailable = 2;
+        // Inventory
+        RoomInventory inventory = new RoomInventory();
 
-        System.out.println("===== ROOM DETAILS =====\n");
-
+        // Single Room
         System.out.println("Single Room:");
         singleRoom.displayRoomDetails();
-        System.out.println("Available: " + singleRoomAvailable);
+        System.out.println("Available Rooms: " +
+                inventory.getAvailability("Single Room"));
         System.out.println();
 
+        // Double Room
         System.out.println("Double Room:");
         doubleRoom.displayRoomDetails();
-        System.out.println("Available: " + doubleRoomAvailable);
+        System.out.println("Available Rooms: " +
+                inventory.getAvailability("Double Room"));
         System.out.println();
 
+        // Suite Room
         System.out.println("Suite Room:");
         suiteRoom.displayRoomDetails();
-        System.out.println("Available: " + suiteRoomAvailable);
+        System.out.println("Available Rooms: " +
+                inventory.getAvailability("Suite Room"));
         System.out.println();
-
-        System.out.println("System started successfully.");
-        System.out.println("Application terminated.");
     }
 }
