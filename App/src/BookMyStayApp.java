47d9d07ccd4a3305eac440659b745014814a2e93
Ramesh Chettiar <rameshchettiar06@gmail.com>
2026-03-25 14:38:22 +0530

    UC7 - Add-On Service Selection

--- App/src/BookMyStayApp.java
+++ App/src/BookMyStayApp.java
@@ -1,33 +1,40 @@
-/**
- * MAIN CLASS - BookMyStayApp
- *
- * Use Case 6: Reservation Confirmation & Room Allocation
- *
- * @version 6.0
- */
 public class BookMyStayApp {
 
     public static void main(String[] args) {
 
         System.out.println("Room Allocation Processing\n");
 
-        // Inventory
         RoomInventory inventory = new RoomInventory();
-
-        // Queue (from UC5)
         BookingRequestQueue bookingQueue = new BookingRequestQueue();
 
         bookingQueue.addRequest(new Reservation("Abhi", "Single Room"));
         bookingQueue.addRequest(new Reservation("Subha", "Single Room"));
         bookingQueue.addRequest(new Reservation("Vanmathi", "Suite Room"));
 
-        // Allocation service
         RoomAllocationService allocationService = new RoomAllocationService();
 
-        // Process requests (FIFO)
+        // UC7: Add-On Service Manager
+        AddOnServiceManager serviceManager = new AddOnServiceManager();
+
         while (bookingQueue.hasPendingRequests()) {
+
             Reservation request = bookingQueue.getNextRequest();
+
             allocationService.allocateRoom(request, inventory);
+
+            // Fetch generated Room ID
+            String roomId = allocationService.getRoomIdForGuest(request.getGuestName());
+
+            if (roomId != null) {
+
+                // Attach services AFTER confirmation
+                serviceManager.addService(roomId, new AddOnService("Breakfast", 500));
+                serviceManager.addService(roomId, new AddOnService("Spa", 800));
+
+                double totalCost = serviceManager.calculateTotalServiceCost(roomId);
+
+                System.out.println("Add-On Cost for " + roomId + ": " + totalCost);
+            }
         }
     }
 }
